9f05552557d3c7be962ac2b9a4e89e27f82b5db6
Richard Oluwadamilola <oluwatobyrichard@gmail.com>
2023-04-17 14:54:13 +0100

    still checking

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,10 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const employees = [];
-
-  // eslint-disable-next-line no-restricted-syntax
-  for (const employee of reportWithIterator) {
-    employees.push(employee);
-  }
-
-  return employees.join(' | ');
+  return reportWithIterator.join(' | ');
 }
