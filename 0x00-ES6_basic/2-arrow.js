07eaa7434b83ad17e761698c3415c5337ddce352
Richard Oluwadamilola <oluwatobyrichard@gmail.com>
2023-04-17 13:57:12 +0100

    Update 2-arrow.js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,9 @@
-export default function getNeighbourhoodsList() {
-	this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-	const self = this;
-	this.addNeighbourhood = (newNeighbourhood) => {
-		self.sanFranciscoNeighborhoods.push(newNeighborhood);
-		return self.sanFranciscoNeighborhoods;
-	};
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
 }
