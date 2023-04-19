fe9817c3fa2ad4abddf3affec1eedbae9991df19
Richard Oluwadamilola <oluwatobyrichard@gmail.com>
2023-04-19 11:04:13 +0100

    update

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-	return Promise.all([uploadPhoto(), createUser()])
-	.then(([body, fullName]) => console.log(`${body.body} ${fullName.firstName}
-	.catch(() => console.log('Signup system offline'));
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([body, fullName]) => console.log(`${body.body} ${fullName.firstName} ${fullName.lastName}`))
+    .catch(() => console.log('Signup system offline'));
 }
