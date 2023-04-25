6725302be6e32acd794202c3ae41c963314a1668
richardoluwadamilola <oluwatobyrichard@gmail.com>
2023-04-25 09:33:41 +0000

    fix

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,8 @@
-const createInt8TypedArray = (length, position, value) => {
-  const buffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(buffer);
-  if (position > int8Array.length) throw new Error('Position outside range');
-  int8Array[position] = value;
-  return new DataView(buffer);
-};
-
-export default createInt8TypedArray;
-
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buf = new DataView(new ArrayBuffer(length), 0, length);
+  buf.setInt8(position, value);
+  return buf;
+}
